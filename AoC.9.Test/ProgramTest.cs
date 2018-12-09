90c62709134e019db1660227ff715f4502260f3d
Unknown <Klaus.Pruenster@gmail.com>
2018-12-09 16:02:28 +0100

    added day 9 structure

---
+++ AoC.9.Test/ProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using AoC._9;
+
+namespace AoC._9.Test
+{
+	[TestFixture]
+	public class ProgramTest
+	{
+		[TestCase(10, 1618, ExpectedResult = 8317)]
+		[TestCase(13, 7999, ExpectedResult = 146373)]
+		[TestCase(17, 1104, ExpectedResult = 2764)]
+		[TestCase(21, 6111, ExpectedResult = 54718)]
+		[TestCase(30, 5807, ExpectedResult = 37305)]
+		public object CalculateWinningElveScore_TakesParamAndReturnsWinningElveScore(int nrElves, int nrMarbles)
+		{
+			return Program.CalculateWinningElveScore(nrElves, nrMarbles);
+		}
+	}
+}
---
+++ AoC.9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AoC._9
+{
+	public class Program
+	{
+		public static int CalculateWinningElveScore(int nrElves, int nrMarbles)
+		{
+			var marbleCircle = new List<int>();
+			var playerScore = new int[nrElves];
+
+			var currentPlayer = 0;
+			var currentMarble = 0;
+			marbleCircle.Add(currentMarble);
+
+			Console.WriteLine("[-] (0)");
+			for (var i = 1; i < nrMarbles - 1; ++i)
+			{
+				currentMarble = ((currentMarble + 1) % marbleCircle.Count) + 1;
+				marbleCircle.Insert(currentMarble, i);
+
+
+				Console.Write($"[{currentPlayer}]");
+
+				foreach (var marble in marbleCircle)
+				{
+					if (marble == i)
+					{
+						Console.Write($"({marble.ToString()})".PadLeft(4));
+					}
+					else
+					{
+						Console.Write(marble.ToString().PadLeft(4));
+					}
+				}
+
+				Console.WriteLine();
+
+				currentPlayer++;
+				currentPlayer %= nrElves;
+			}
+
+
+			return 123;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Advent of Code Day 9!");
+
+			CalculateWinningElveScore(9, 25);
+
+			Console.ReadLine();
+		}
+	}
+}
