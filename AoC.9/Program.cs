41c9f370c6ce18e3f4ae3f2dfd71a58e0d27d9b4
Unknown <Klaus.Pruenster@gmail.com>
2018-12-10 18:34:42 +0100

    speeded up day 9

--- AoC.9/Program.cs
+++ AoC.9/Program.cs
@@ -5,56 +5,62 @@ using System.Numerics;
 
 namespace AoC._9
 {
-	public class Program
+	public static class Program
 	{
+		public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
+		{
+			if (current.Next == null)
+				return current.List.First;
+			return current.Next;
+		}
+
+		public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current)
+		{
+			if (current.Previous == null)
+				return current.List.Last;
+			return current.Previous;
+		}
+
+		public static LinkedListNode<T> GetNodeAtPos<T>(this LinkedListNode<T> currentNode, int shiftDelta)
+		{
+			for (int i = 0; i < Math.Abs(shiftDelta); i++)
+			{
+				if (shiftDelta < 0)
+				{
+					currentNode = currentNode.PreviousOrLast();
+				}
+				else if (shiftDelta > 0)
+				{
+					currentNode = currentNode.NextOrFirst();
+				}
+			}
+
+			return currentNode;
+		}
+
 		public static BigInteger CalculateWinningElveScore(int nrElves, int nrMarbles)
 		{
-			var marbleCircle = new List<int>();
+			var marbleCircle = new LinkedList<int>();
 			var playerScore = new BigInteger[nrElves];
 
 			var currentPlayer = 0;
-			var currentMarble = 0;
-			marbleCircle.Add(currentMarble);
-
-			if (nrMarbles < 30 && nrElves < 10)
-				Console.WriteLine("[-] (0)");
+			var currentMarble = marbleCircle.AddFirst(0);
 
 			for (var i = 1; i <= nrMarbles; ++i)
 			{
 				if (i % 23 == 0)
 				{
-					playerScore[currentPlayer] += i;
-					currentMarble = currentMarble - 7;
-					if (currentMarble < 0)
-						currentMarble = marbleCircle.Count + currentMarble;
+					currentMarble = currentMarble.GetNodeAtPos(-7);
+					playerScore[currentPlayer] += currentMarble.Value;
 
-					playerScore[currentPlayer] += marbleCircle[currentMarble];
-
-					marbleCircle.RemoveAt(currentMarble);
+					var nextMarble = currentMarble.Next;
+					marbleCircle.Remove(currentMarble);
+					currentMarble = nextMarble;
 				}
 				else
 				{
-					currentMarble = ((currentMarble + 1) % marbleCircle.Count) + 1;
-					marbleCircle.Insert(currentMarble, i);
-				}
-
-				if (nrMarbles < 30 && nrElves < 10)
-				{
-					Console.Write($"[{currentPlayer}]");
-
-					foreach (var marble in marbleCircle)
-					{
-						if (marble == i)
-						{
-							Console.Write($"({marble.ToString()})".PadLeft(4));
-						}
-						else
-						{
-							Console.Write(marble.ToString().PadLeft(4));
-						}
-					}
-
-					Console.WriteLine();
+					currentMarble = currentMarble.NextOrFirst();
+					currentMarble = marbleCircle.AddAfter(currentMarble, i);
 				}
 
 				currentPlayer++;
@@ -72,7 +78,6 @@ namespace AoC._9
 			Console.WriteLine("Example:");
 			Console.WriteLine($"Example score: {CalculateWinningElveScore(9, 25)}");
 			Console.WriteLine($"Star 1: {CalculateWinningElveScore(430, 71588)}");
-			Console.WriteLine("Calculation for Star 2 takes 4-ever. Here is the pre-calculated value: 3412522480");
 			Console.WriteLine($"Star 2: {CalculateWinningElveScore(430, 71588 * 100)}");
 
 			Console.ReadLine();
