02acc6e22bf28d05726afd5590648840d8968a5f
Unknown <Klaus.Pruenster@gmail.com>
2018-12-11 20:04:31 +0100

    added day 11 structure

---
+++ AoC.11.Test/ProgramTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AoC._11.Test
+{
+	[TestFixture]
+	public class ProgramTest
+	{
+		[TestCase(8, 3, 5, ExpectedResult = 4)]
+		[TestCase(57, 122, 79, ExpectedResult = -5)]
+		[TestCase(39, 217, 196, ExpectedResult = 0)]
+		[TestCase(71, 101, 153, ExpectedResult = 4)]
+		public object CalculateMatrix_TakesParameterAndReturnsMatrixAtPos(int serialNr, int x, int y)
+		{
+			var grid = Program.CalculateMatrix(serialNr);
+
+			return grid[x, y];
+		}
+	}
+}
---
+++ AoC.11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace AoC._11
+{
+	public class Program
+	{
+		public static int PowerGridSerialNumber { get; } = 8772;
+
+		public static int[,] CalculateMatrix(int serialNr)
+		{
+			var matrix = new int[300, 300];
+
+			for (var x = 0; x < 300; x++)
+			{
+				for (var y = 0; y < 300; y++)
+				{
+					var rackId = (x + 10);
+
+					var intermediate = (rackId * y + serialNr) * rackId;
+					var stringNr = intermediate.ToString();
+					var hundredDigit = stringNr.Length >= 3 ? int.Parse(stringNr.ElementAt(stringNr.Length - 3).ToString()) : 0;
+					matrix[x, y] = hundredDigit - 5;
+				}
+			}
+
+			return matrix;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Advent of Code Day 10!");
+
+			var matrix = CalculateMatrix(PowerGridSerialNumber);
+
+			Console.ReadLine();
+		}
+	}
+}
